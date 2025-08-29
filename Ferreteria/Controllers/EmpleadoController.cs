0cb3f5d3e82c745f66e2d561c2fc1b326653608a
josemmcampos <josemmcampos77@gmail.com>
2025-08-28 22:55:44 -0600

    Se pudo

---
+++ Ferreteria/Controllers/EmpleadoController.cs
@@ -0,0 +1,18 @@
+﻿// Controllers/EmpleadoController.cs
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // Acción para mostrar la lista de empleados
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Puedes agregar más acciones si necesitas CRUD
+        // Ejemplo: Create, Edit, Delete, Details
+    }
+}
